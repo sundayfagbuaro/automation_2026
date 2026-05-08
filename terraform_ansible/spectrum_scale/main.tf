e1a5e4cc5c53f1a390a5de68e6a56b82fb82c334
fabuu4real@gmail.com <fabuu4real@gmail.com>
2026-05-08 21:15:58 +0100

    reduced vm count to 1

--- terraform_ansible/spectrum_scale/main.tf
+++ terraform_ansible/spectrum_scale/main.tf
@@ -8,8 +8,8 @@ resource "proxmox_vm_qemu" "vm" {
   agent                  = 1
   define_connection_info = true
   ci_wait                = 60
-#  ciuser = "bobosunne"
-#  cipassword = "TempPassword"
+  ciuser = "bobosunne"
+  cipassword = "password"
   sshkeys    = file("/home/bobosunne/.ssh/id_rsa.pub")
 
   os_type = "cloud-init"
