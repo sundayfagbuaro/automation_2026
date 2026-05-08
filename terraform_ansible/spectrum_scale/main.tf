7083261609e8ed1bbeb121085dc8acd481c391c2
fabuu4real@gmail.com <fabuu4real@gmail.com>
2026-05-08 20:09:54 +0100

    reduced vm count to 1

--- terraform_ansible/spectrum_scale/main.tf
+++ terraform_ansible/spectrum_scale/main.tf
@@ -1,5 +1,5 @@
 resource "proxmox_vm_qemu" "vm" {
-  count       = 3
+  count       = 1
   name        = "${var.vm_name}-${count.index + 1}"
   target_node = var.target_node
   clone       = var.template_name
