0c59c886de1dd760f04eadbc42dec4506f51981f
fabuu4real@gmail.com <fabuu4real@gmail.com>
2026-05-13 23:07:32 +0100

    corrected VM provisioning block

--- terraform_ansible/spectrum_scale/main.tf
+++ terraform_ansible/spectrum_scale/main.tf
@@ -5,7 +5,7 @@ resource "proxmox_vm_qemu" "vm" {
   clone       = var.template_name
   full_clone  = true
 
-  vmid        = 200 + count.index
+  vmid        = 400 + count.index
   agent                  = 1
   define_connection_info = true
   ci_wait                = 60
