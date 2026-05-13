4b0cf5b80d60ba4ff0952ebf967f955b16e771b7
fabuu4real@gmail.com <fabuu4real@gmail.com>
2026-05-13 16:22:37 +0100

    corrected VM provisioning block

--- terraform_ansible/spectrum_scale/main.tf
+++ terraform_ansible/spectrum_scale/main.tf
@@ -12,12 +12,13 @@ resource "proxmox_vm_qemu" "vm" {
   cipassword = "password"
   sshkeys    = file("/home/bobosunne/.ssh/id_rsa.pub")
 
+  # Additional disk
   disk {
-  slot    = 1
-  size    = "20G"
-  type    = "scsi"
-  storage = "local-lvm"
-}
+    slot    = "scsi1"
+    type    = "disk"
+    storage = "local-lvm"
+    size    = "25G"
+  }
 
   os_type = "cloud-init"
 
